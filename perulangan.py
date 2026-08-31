51f5ac3e6fa96e2a665f336fee8d61aa4089c1b7
18-ZIDAN-RIZQULLAH <woinjay160@gmail.com>
2026-08-31 09:14:06 +0700

    Delete perulangan.py

--- perulangan.py
+++
@@ -1,6 +0,0 @@
-while True:
-    x = int(input("Masukkan Nilai X: "))
-    if x % 2 == 0:
-        print(f"Bilangan {x} termasuk Bilangan genap")
-    else:
-        print(f"Bilangan {x} termasuk Bilangan ganjil")
